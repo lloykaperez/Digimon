866349a9e565f9edcf9872e2cd244ccb36bb9a28
Andrea (Lloyka) Pérez <andreaperezharambour@gmail.com>
2023-04-18 21:31:49 -0400

    Segunda versión Digimon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 const listaDigimon = document.querySelector("#listaDigimon");
+const botonesHeader = document.querySelectorAll(".btn-header");
+const buscador = document.querySelector("#buscador");
+const URL = "https://digimon-api.vercel.app/api/digimon";
 
-let URL = "https://digimon-api.vercel.app/api/digimon";
-
-
+// Obtener todos los digimones
 fetch(URL)
   .then(response => response.json())
   .then(data => {
@@ -11,29 +12,83 @@ fetch(URL)
     });
   });
 
-
+// Función para mostrar un solo digimón
 function mostrarDigimon(digi) {
+  const div = document.createElement("div");
+  div.classList.add("digimon");
+  div.innerHTML = `
+    <p class="digimon-level-back">${digi.level}</p>
+    <div class="digimon-imagen">
+      <img src="${digi.img}" alt="${digi.name}">
+    </div>
+    <div class="digimon-info">
+      <div class="nombre-contenedor">
+        <h2 class="digimon-nombre">${digi.name}</h2>
+      </div>
+      <div class="digimon-level">
+        <p class="${digi.level.toLowerCase().split(' ').join('-')} level">${digi.level}</p>
+      </div>
+    </div>
+  `;
+  listaDigimon.append(div);
+}
 
-    let levels = digi.level;
-    console.log(levels);
+// Función para filtrar los digimones por nivel
+function filtrarDigimonPorNivel(nivel) {
+  // Vaciar la lista de digimones para mostrar solo los que corresponden al nivel seleccionado
+  listaDigimon.innerHTML = "";
 
-    const div = document.createElement("div");
-    div.classList.add("digimon");
-    div.innerHTML = `
-        <p class="digimon-level-back">${digi.level}</p>
-        <div class="digimon-imagen">
-            <img src="${digi.img}" alt="${digi.name}">
-        </div>
-        <div class="digimon-info">
-            <div class="nombre-contenedor">
-                <h2 class="digimon-nombre">${digi.name}</h2>
-            </div>
-            <div class="digimon-level">
-                <p class="${digi.level.toLowerCase().replace(' ', '+')} level">${digi.level}</p>
-            </div>
-        </div>
-    `;
-    listaDigimon.append(div);
+  // Obtener los digimones que corresponden al nivel seleccionado
+  fetch(`https://digimon-api.vercel.app/api/digimon/level/${nivel}`)
+    .then(response => response.json())
+    .then(data => {
+      data.forEach(digi => {
+        mostrarDigimon(digi);
+      });
+    });
 }
 
 
+botonesHeader.forEach(boton => boton.addEventListener("click", (event) => {
+    const botonId = event.currentTarget.id;
+
+    listaDigimon.innerHTML = "";
+
+    if (botonId === "ver-todos") {
+        fetch(URL)
+            .then(response => response.json())
+            .then(data => {
+                data.forEach(digi => {
+                    mostrarDigimon(digi);
+                });
+            });
+    } else {
+        fetch(`https://digimon-api.vercel.app/api/digimon/level/${botonId}`)
+            .then(response => response.json())
+            .then(data => {
+                data.forEach(digi => {
+                    mostrarDigimon(digi);
+                });
+            });
+    }
+}));
+
+
+buscador.addEventListener("input", (event) => {
+  const busqueda = event.target.value.toLowerCase();
+  filtrarDigimonPorNombre(busqueda);
+});
+
+function filtrarDigimonPorNombre(nombre) {
+  listaDigimon.innerHTML = "";
+
+  fetch(URL)
+    .then(response => response.json())
+    .then(data => {
+      data.forEach(digi => {
+        if (digi.name.toLowerCase().includes(nombre)) {
+          mostrarDigimon(digi);
+        }
+      });
+    });
+}
